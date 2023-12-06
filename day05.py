5cff1f3f5b62a8a2ccc22245046e5e5cae43ed02
Nicolas M <mine.nicolas@gmail.com>
2023-12-06 20:48:35 +0100

    add comments in day 5 + add O(1) solution for day 6

--- day05.py
+++ day05.py
@@ -1,32 +1,42 @@
-def get_next_range(source, ranges):
-    ans = []
+def get_next_range(source: list[list[int]], ranges: list[list[int]]) -> list[list[int]]:
+    """
+    Project each source range (x_start, e_end) onto the target ranges (y_start, y_end)
+    Those new ranges are the new source ranges (x_start, x_end) for the next projection
+    In case of overlap, split the source range into two or three parts.
+    """
+    target = []
     for start, end in source:
-        for y, x, z in ranges:
+        for init_y, init_x, range_width in ranges:
             # check if there is overlap and split if needed
-            new_start = max(start, x)
-            new_end = min(end, x + z - 1)
-            if new_start <= new_end:
-                ans.append((new_start - x + y, new_end - x + y))
+            new_start = max(start, init_x)
+            new_end = min(end, init_x + range_width - 1)
+            if new_start <= new_end:  # means overlap exists
+                target.append((new_start - init_x + init_y, new_end - init_x + init_y))
                 if new_start > start:
-                    ans.append((start, new_start - 1))
+                    target.append((start, new_start - 1))
                 if new_end < end:
-                    ans.append((new_end + 1, end))
+                    target.append((new_end + 1, end))
                 break
-        else:
-            ans.append((start, end))
-    return ans
+        else:  # no overlap found in any range (for-else loop, not if-else)
+            target.append((start, end))
+    return target
 
 
-def parse_stage(stage):
-    sections = stage.split("\n")[1:]
+def parse_stage(stage: str) -> list[list[int]]:
+    """
+    Parse a section of the input into a list of ranges
+    """
     ranges = []
-    for section in sections:
+    for section in stage.split("\n")[1:]:
         y, x, z = [int(x) for x in section.split()]
         ranges.append((y, x, z))
     return ranges
 
 
-def parse_seed(seed, version=1):
+def parse_seed(seed: str, version=1) -> list[list[int]]:
+    """
+    Process the seed row into a list of ranges (start, end) instead of (start, offset)
+    """
     if version == 1:
         init_seeds = []
         for x in seed[7:].split():
@@ -44,12 +54,12 @@ if __name__ == "__main__":
     with open("input05.txt", "r") as f:
         lines = f.read().split("\n\n")
 
-    source = parse_seed(lines[0], version=2)
+    source = parse_seed(lines[0], version=1)
     # print(source)
 
     for stage in lines[1:]:
-        # print(source)
         ranges = parse_stage(stage)
         source = get_next_range(source, ranges)
+        # print(source)
 
     print(min([x[0] for x in source]))
--- day06.py
+++ day06.py
@@ -1,3 +1,10 @@
+import math
+
+
+def clamp(n, smallest, largest):
+    return max(smallest, min(n, largest))
+
+
 def read_input(lines, version=1):
     if version == 1:
         times = [int(x) for x in lines[0].split(":")[1].split()]
@@ -15,8 +22,21 @@ with open('input06.txt') as f:
 times, distances = read_input(lines, version=2)
 print(times, distances)
 
+# V1
 ans = 1
 for time, distance in zip(times, distances):
     ans *= sum((time * hold) - (hold**2) > distance for hold in range(time))
 
+print(ans)
+
+# V2
+ans = 1
+for time, distance in zip(times, distances):
+    a, b, c = -1, time, -distance
+    delta = b**2 - 4*a*c
+    r1 = (-b + delta**0.5) / (2*a)  # 0 < r1 < r2 < ???
+    r2 = (-b - delta**0.5) / (2*a)
+    t1 = clamp(math.ceil(r1), 0, time)  # take the first next integer as the start time cannot be float
+    t2 = clamp(math.floor(r2), 0, time) # take the last previous integer as the end time cannot be float
+    ans *= t2-t1+1
 print(ans)

e5684526a861c78795ea1e3fa2c9f02e2c2beaca
Nicolas M <mine.nicolas@gmail.com>
2023-12-06 07:14:45 +0100

    add part2

--- day06.py
+++ day06.py
@@ -1,9 +1,18 @@
+def read_input(lines, version=1):
+    if version == 1:
+        times = [int(x) for x in lines[0].split(":")[1].split()]
+        distances = [int(x) for x in lines[1].split(":")[1].split()]
+        return times, distances
+    else:
+        times = [int(lines[0].split(":")[1].replace(" ", "").replace("\n", ""))]
+        distances = [int(lines[1].split(":")[1].replace(" ", "").replace("\n", ""))]
+        return times, distances
+    
+
 with open('input06.txt') as f:
     lines = f.readlines()
 
-times = [int(x) for x in lines[0].split(":")[1].split()]
-distances = [int(x) for x in lines[1].split(":")[1].split()]
-
+times, distances = read_input(lines, version=2)
 print(times, distances)
 
 ans = 1

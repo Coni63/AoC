0bdc04a90841069a5a3df1653ae375385b6d9171
Nicolas M <mine.nicolas@gmail.com>
2023-12-06 07:08:30 +0100

    add day 6

--- day06.py
+++ day06.py
@@ -1,4 +1,13 @@
 with open('input06.txt') as f:
     lines = f.readlines()
-    for line in lines:
-        pass
+
+times = [int(x) for x in lines[0].split(":")[1].split()]
+distances = [int(x) for x in lines[1].split(":")[1].split()]
+
+print(times, distances)
+
+ans = 1
+for time, distance in zip(times, distances):
+    ans *= sum((time * hold) - (hold**2) > distance for hold in range(time))
+
+print(ans)

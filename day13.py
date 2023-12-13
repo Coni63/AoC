8660881c79c15c24ba6fc220927c1aa5589d7927
Nicolas M <mine.nicolas@gmail.com>
2023-12-13 18:46:15 +0100

    add part 2 and refactoring

--- day13.py
+++ day13.py
@@ -1,18 +1,27 @@
 import numpy as np
 
 
-def find_symetry(g):
+def find_symetry(g: list[list[int]], accepted_errors: int) -> int:
+    """
+    Return the vertical index of a mirror
+    Index starts at 1 but return 0 if no mirror
+    """
     H, W = g.shape
 
-    for i in range(W-1):
-        offset = 0
-        while True:
-            if (i-offset) < 0 or (i+offset+1) >= W:
-                return i+1
-            if np.array_equal(g[:, i-offset], g[:, i+offset+1]):
-                offset += 1
-            else:
-                break
+    for col in range(1, W):
+        max_width = min(col, W-col)
+
+        start = col-max_width
+        mid = col
+        end = col+max_width
+
+        C1 = g[:, start:mid]
+        C2 = np.fliplr(g[:, mid:end])
+
+        mismatch = np.abs(C1 - C2).sum()
+
+        if mismatch == accepted_errors:
+            return col
 
     return 0
 
@@ -20,18 +29,14 @@ def find_symetry(g):
 with open('input13.txt') as f:
     puzzles = f.read().split("\n\n")
 
+VERSION = 1
 ans = 0
+
+errors = 0 if VERSION == 1 else 1
 for puzzle in puzzles:
     g = np.array([[1 if x == "#" else 0 for x in row] for row in puzzle.split("\n")])
 
-    # print(g)
-
-    a = find_symetry(g)
-    b = find_symetry(g.T)
-
-    ans += a + 100 * b
-
-    # print(g.shape)
-    # print(a, b)
+    ans += find_symetry(g, accepted_errors=errors)
+    ans += 100 * find_symetry(g.T, accepted_errors=errors)
 
 print(ans)
